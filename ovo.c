a418c47d0e27239ab23e099c333595ba97bda185
Yasmim Silva Maciel <yasmim.maciel@upe.br>
2025-10-25 23:15:55 -0300

    MICHELANGELO: Ao invés de mostrar o vetor, mostra o tabuleiro com colchete para o usuário

--- ovo.c
+++ ovo.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 // declara tamanho de uma cosntante que usaremos apra ser o array do OVO
 #define OVO 15
 
-//fazemos uma função para printar o vetor como queremos na tela.
-void definirVetor(int vetor[], int tamanho){
 
-    for (int i = 0; i<tamanho; i++){
-        if (vetor[i]==0) printf("[  ]");
-        else printf("[%d]", vetor[i]);
+// uma função que irá converter o vetor aos olhares do usuarios para colchetes, para ficar mais nitido o tabuleiro
+
+void definirVetor(char vetor[], int tamanho) {
+    printf("\n");
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] == '.') printf("[ ]");
+        else printf("[%c]", vetor[i]);
     }
-
+    printf("\n");
 }
+
 //função principal
 int main(){
     int local;
